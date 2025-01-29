93ef235018fc7e453be694e83ae505ba496f3f39
Eric Wong <wsdjeg@outlook.com>
2025-01-29 19:47:56 +0800

    enable signcolumn by default

--- lua/flygrep.lua
+++ lua/flygrep.lua
@@ -95,6 +95,7 @@ local function open_win()
   vim.api.nvim_set_option_value('number', false, { win = prompt_winid })
   vim.api.nvim_set_option_value('relativenumber', false, { win = prompt_winid })
   vim.api.nvim_set_option_value('cursorline', false, { win = prompt_winid })
+  vim.api.nvim_set_option_value('signcolumn', 'yes', { win = prompt_winid })
   vim.api.nvim_buf_set_extmark(prompt_bufid, extns, 0, 0, {
     sign_text = '>',
     sign_hl_group = 'Error',
