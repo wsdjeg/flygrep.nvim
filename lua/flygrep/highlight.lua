50fe5e265e3879ec476781753d9c502f3c7c72a6
Eric Wong <wsdjeg@outlook.com>
2025-04-18 19:45:21 +0800

    setup before open win

--- lua/flygrep/highlight.lua
+++ lua/flygrep/highlight.lua
@@ -1,9 +1,6 @@
 local M = {}
-
-local color_templete = require('flygrep.config').color_templete
 local util = require('flygrep.util')
-
-function M.def_higroup()
+function M.def_higroup(color_templete)
   vim.api.nvim_set_hl(0, 'FlyGrep_a', color_templete.a)
   vim.api.nvim_set_hl(0, 'FlyGrep_b', color_templete.b)
   util.hi_separator('FlyGrep_a', 'FlyGrep_b')
