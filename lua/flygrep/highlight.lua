325c3768a47e953925c5451ca80a1f038806a011
Eric Wong <wsdjeg@outlook.com>
2025-04-18 19:36:34 +0800

    remove spacevim api

--- lua/flygrep/highlight.lua
+++ lua/flygrep/highlight.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 local color_templete = require('flygrep.config').color_templete
-local hi = require('spacevim.api.vim.highlight')
+local util = require('flygrep.util')
 
 function M.def_higroup()
   vim.api.nvim_set_hl(0, 'FlyGrep_a', color_templete.a)
   vim.api.nvim_set_hl(0, 'FlyGrep_b', color_templete.b)
-  hi.hi_separator('FlyGrep_a', 'FlyGrep_b')
-  hi.hi_separator('FlyGrep_b', 'Normal')
+  util.hi_separator('FlyGrep_a', 'FlyGrep_b')
+  util.hi_separator('FlyGrep_b', 'Normal')
 end
 
 return M
